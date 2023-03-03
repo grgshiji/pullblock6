af7aaf6b2ba43e52784f195b58a292deae1035ab
grgshiji <shiji@neoito.com>
2023-03-03 09:37:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const handler = async (event) => {
+
+  const {req, res} = event
+  
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
+  res.end()
+  
+}
+
+export default handler
